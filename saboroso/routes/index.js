89b3b8bcc358ec6f45b7e811873aa82979f8a464
Fabio Gilberto <fabio.goncalves@bomjesus.br>
2022-02-16 13:16:56 -0300

    Reestruturacao por meio de pastas includes

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -20,24 +20,32 @@ router.get('/', function(req, res, next) {
 router.get('/menu', function(req, res, next) {
     res.render('menu', { //nome do arquivo dentro da pasta view
       title: 'Menu - Restaurante saboroso',
+      background:'images/img_bg_1.jpg',
+      h1: "Saboreie nosso menu!",
     });
 });
 
 router.get('/reservation', function(req, res, next) {
   res.render('reservation', { //nome do arquivo dentro da pasta view
     title: 'Reserva - Restaurante saboroso',
+    background:'images/img_bg_2.jpg',
+    h1: "Reserve uma Mesa!",
   });
 });
 
 router.get('/services', function(req, res, next) {
   res.render('services', { //nome do arquivo dentro da pasta view
     title: 'Services - Restaurante saboroso',
+    background:'images/img_bg_1.jpg',
+    h1: "É um prazer poder servir!",
   });
 });
 
 router.get('/contact', function(req, res, next) {
   res.render('contact', { //nome do arquivo dentro da pasta view
     title: 'Contact - Restaurante saboroso',
+    background:'images/img_bg_3.jpg',
+    h1: "Diga um oi!",
   });
 });
 
